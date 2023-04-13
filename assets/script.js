cd3bfcd98da3cc9f4b38be5ca5d7fb12316a0e10
antoninodangelo11 <reapercsp@gmail.com>
2023-04-13 17:48:14 +0200

    add input js

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,42 @@
 /*START JS*/
 
+/*DICHIARAZIONE VARIABILI*/
+
+let userNameElement = document.querySelector('.user_name');
+let userDistanceElement = document.querySelector('.user_distance');
+let userAgeElement = document.querySelector('.user_age');
 const priceForKm = 0.21;
 const discountChildren = 20;
 const discountSenior = 40;
+// console.log(userNameElement, userDistanceElement, userAgeElement);
 
-const userName = document.querySelector('.user_name');
-const userDistance = document.querySelector('.user_distance');
-const userAge = document.querySelector('.user_Age');
-// console.log(userName, userDistance, userAge);
+/*INZIO INPUT*/
 
-const basePrice = userDistance * priceForKm;
-// console.log(basePrice);
+btn_active.addEventListener('click', function () {
 
-let discountPercent = 0;
+    const userName = parseInt(userNameElement.value);
+    const userDistance = parseInt(userDistanceElement.value);
+    const userAge = parseInt(userAgeElement.value);
 
-if (userAge < 18) {
+    const basePrice = userDistance * priceForKm;
+    // console.log(basePrice);
+
+    let discountPercent = 0;
+
+    if (userAge < 18) {
     discountPercent = discountChildren;
-} else if (age >= 65) {
+    } else if (userAge >= 65) {
 	discountPercent = discountSenior;
-}
+    }
+    // console.log(discountPercent);
 
-// console.log(discountPercent);
+    const discount = basePrice * discountPercent / 100;
+    // console.log(discount);
 
-const discount = basePrice * discountPercent / 100;
-// console.log(discount);
+    let finalPrice = basePrice - discount;
+    finalPrice = parseFloat(finalPrice.toFixed(2));
+    document.getElementById('final_price').innerHTML = ('' + finalPrice);
+    // console.log(finalPrice);
 
-let finalPrice = basePrice - discount;
-finalPrice = parseFloat(finalPrice.toFixed(2));
-// console.log(finalPrice);
-
-
-
+} );
 
